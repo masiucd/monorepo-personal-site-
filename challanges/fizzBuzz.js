e608f8b5db30cd7b22c4fc9bc7ae596e5f163360
Masiu <ciszekmarcell@gmail.com>
2019-12-06 15:46:58 +0100

    fizzbuzz

---
+++ challanges/fizzBuzz.js
@@ -0,0 +1,16 @@
+const fizzBuzz = n => {
+  for (let i = 0; i < n; i += 1) {
+    if (i % 5 === 0 && i % 3 === 0) {
+      console.log('FizzBuzz');
+    } else if (i % 5 === 0) {
+      console.log('Fizz');
+    } else if (i % 3 === 0) {
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }
+};
+
+const print = fizzBuzz(50);
+console.log(print);
