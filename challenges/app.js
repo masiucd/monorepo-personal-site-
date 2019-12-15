a081e16259e92556f37087043a3a01a8d2553cfa
Masiu <ciszekmarcell@gmail.com>
2019-12-15 10:21:30 +0100

    tests

--- challenges/app.js
+++ challenges/app.js
@@ -18,7 +18,24 @@ const app = (str1, str2) => {
   return true;
 };
 
-console.log(app('aaz', 'zza'));
-console.log(app('masiiu', 'aisumi'));
+// console.log(app('aaz', 'zza'));
+// console.log(app('masiiu', 'aisumi'));
 // console.log(app('car', 'rat'));
 // console.log(app('', ''));
+
+/**
+ * @param {number[]} arr
+ * @returns {number[]}
+ */
+
+const app2 = arr => {
+  for (let i = 0; i < arr.length; i += 1) {
+    for (let j = 0; j < arr.length; j += 1) {
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+};
+
+console.log(app2([-4, -3, -2, -1, 0, 1, 2, 3, 4]));
