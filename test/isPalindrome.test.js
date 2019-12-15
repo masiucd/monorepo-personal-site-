c68bfaafbb10e12cae819965cb39439337803ef3
Masiu <ciszekmarcell@gmail.com>
2019-12-15 11:54:59 +0100

    tests

--- test/isPalindrome.test.js
+++
@@ -1,7 +0,0 @@
-// const { isPalindrome } = require('../challanges/isPalindrome');
-
-const { isPalindrome } = require('../challenges/isPalindrome');
-
-test('is valid palindome', () => {
-  expect(isPalindrome('apa')).toBe(true);
-});
--- test/sumZero.test.js
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-const { sumZero, sumZero2 } = require('../challenges/sumZero');
-
-test('to be 0', () => {
-  const arr = [-3, -2, -1, 0, 1, 2, 3];
-  expect(sumZero(arr)).toEqual([-3, 3]);
-  expect(sumZero2(arr)).toEqual([-3, 3]);
-});
