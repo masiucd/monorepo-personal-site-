22620ed9ba16b2c61387806174a90bfe03617161
Masiu <ciszekmarcell@gmail.com>
2019-12-15 10:08:19 +0100

    fizz buzz

--- challenges/fizzBuzz.js
+++ challenges/fizzBuzz.js
@@ -16,8 +16,23 @@ const fizzBuzz = n => {
     }
   }
 };
+const fizzBuzz2 = n => {
+  const arr = [];
+  for (let i = 0; i < n; i += 1) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      arr.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      arr.push('Fizz');
+    } else if (i % 5 === 0) {
+      arr.push('Buzz');
+    } else {
+      arr.push(i.toString());
+    }
+  }
+  return arr;
+};
 
-const print = fizzBuzz(10);
+const print = fizzBuzz2(10);
 console.log(print);
 
 module.exports = {
