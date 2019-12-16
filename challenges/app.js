ccf60c44c524eae1d6abaed0d935e8449b9d3b03
Masiu <ciszekmarcell@gmail.com>
2019-12-16 14:16:43 +0100

    sliding window

--- challenges/app.js
+++ challenges/app.js
@@ -1,3 +1,6 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable prefer-const */
 /**
  *
  * @param {number} s1
@@ -19,6 +22,56 @@ function sameFrequency(s1, s2) {
   return true;
 }
 
-console.log(sameFrequency(182, 281)); //  true
-console.log(sameFrequency(34, 14)); // fasle
-console.log(sameFrequency(182, 8211)); // fasle
+// console.log(sameFrequency(182, 281)); //  true
+// console.log(sameFrequency(34, 14)); // fasle
+// console.log(sameFrequency(182, 8211)); // fasle
+
+/**
+ *
+ * @param {number[] | string[]} args
+ * @returns {boolean}
+ */
+function areThereDuplicates(...args) {
+  const map = args.reduce((obj, val) => {
+    if (!obj[val]) {
+      obj[val] = 1;
+    } else {
+      obj[val] += 1;
+    }
+    return obj;
+  }, {});
+
+  for (let key in map) {
+    if (map[key] > 1) return true;
+  }
+  return false;
+}
+
+// console.log(areThereDuplicates(1, 2, 3)); // fasle
+// console.log(areThereDuplicates(1, 2, 2)); // true
+// console.log(areThereDuplicates('a', 'b', 'b')); // true
+
+/**
+ *
+ * @param {number[]} arr
+ * @param {number} num
+ * @returns {number}
+ */
+
+function maxSubarraySum(arr, num) {
+  if (arr.length < num) return null;
+
+  let total = 0;
+  for (let i = 0; i < num; i += 1) {
+    total += arr[i];
+  }
+  let acc = total;
+  for (let i = num; i < arr.length; i += 1) {
+    acc += arr[i] - arr[i - num];
+    total = Math.max(total, acc);
+  }
+  return total;
+}
+
+console.log(maxSubarraySum([200, 300, 100, 500], 2));
+console.log(maxSubarraySum([100, 200, 300, 400], 2));
