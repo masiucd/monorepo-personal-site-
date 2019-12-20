a36c0f6751cd0f4073879bf6a3aec8ac613338dd
Masiu <ciszekmarcell@gmail.com>
2019-12-20 13:25:28 +0100

    naive search

---
+++ search/naiveStrSearch/index.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * @param {string} long
+ * @param {string} short
+ * @return {number}
+ */
+function strSearch(long, short) {
+  let count = 0;
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; j < short.length; j++) {
+      if (short[j] !== long[i + j]) break;
+      if (j === short.length - 1) count++;
+    }
+  }
+  return count;
+}
+
+console.log(strSearch('loled', 'lol'));
