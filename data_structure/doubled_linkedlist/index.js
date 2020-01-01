066b3f83274fc0cd23ff699ca1071218755e785c
Masiu <ciszekmarcell@gmail.com>
2020-01-01 15:31:41 +0100

    add adn pop

--- data_structure/doubled_linkedlist/index.js
+++ data_structure/doubled_linkedlist/index.js
@@ -2,6 +2,7 @@ class Node {
   constructor(data) {
     this.data = data;
     this.next = null;
+    this.prev = null;
   }
 }
 
@@ -17,11 +18,42 @@ class LinkedList {
    * @param {string} data
    * @returns {this}
    */
-  insertFirst(data) {
-    //
+  push(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.size += 1;
+    return this;
+  }
+
+  /**
+   * @returns {this}
+   */
+  pop() {
+    if (!this.head) return null;
+    const poppedNode = this.tail;
+    if (this.size === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.size -= 1;
+    return poppedNode;
   }
 }
 
 const ll = new LinkedList();
+
+ll.push('apa');
+ll.push('banan');
 
 console.log(ll);
