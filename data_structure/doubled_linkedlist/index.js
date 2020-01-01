5c6a672036f0088b81c521c94c680084550f321b
Masiu <ciszekmarcell@gmail.com>
2020-01-01 15:08:13 +0100

    doubly linked list

---
+++ data_structure/doubled_linkedlist/index.js
@@ -0,0 +1,27 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  /**
+   *
+   * @param {string} data
+   * @returns {this}
+   */
+  insertFirst(data) {
+    //
+  }
+}
+
+const ll = new LinkedList();
+
+console.log(ll);
