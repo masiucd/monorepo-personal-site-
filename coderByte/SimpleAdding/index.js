289b6cbaa35305d58a4bcb0c100a552655326284
Masiu <ciszekmarcell@gmail.com>
2020-01-11 11:12:35 +0100

    with recursion

--- coderByte/SimpleAdding/index.js
+++ coderByte/SimpleAdding/index.js
@@ -12,5 +12,18 @@ function SimpleAdding(num) {
   return nums.reduce((a, b) => a + b, 0);
 }
 
+/**
+ *
+ * @param {number} num
+ * @returns {number}
+ */
+function SimpleAddingRec(num) {
+  if (num === 1) {
+    return 1;
+  }
+
+  return num + SimpleAddingRec(num - 1);
+}
+
 console.log(SimpleAdding(4)); // 1 + 2 + 3 + 4 = 10.
 console.log(SimpleAdding(12)); // 78
