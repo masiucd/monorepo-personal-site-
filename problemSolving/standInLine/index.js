edc53707e2859c8460fd3985566cd21dcc2907af
Marcell Ciszek <you@example.com>
2020-01-22 10:50:51 +0100

    add

---
+++ problemSolving/standInLine/index.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * @param {number[]} arr
+ * @param {number} num
+ * @returns {number[] | string}
+ */
+const standInLine = (arr, num) => {
+  //
+};
+
+console.log(standInLine([1, 4, 5, 6, 9], 2));
+console.log(standInLine([], 2));
