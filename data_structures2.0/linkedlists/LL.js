7df1d310e0071e620f1fe91374519a98d5b2a818
Masiu <ciszekmarcell@gmail.com>
2020-03-15 12:41:57 +0100

    singly linked list

--- data_structures2.0/linkedlists/LL.js
+++ data_structures2.0/linkedlists/LL.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 class Node {
   constructor(data) {
     this.data = data;
@@ -12,8 +13,18 @@ class LinkedList {
     this.length = 0;
   }
 
-  append(value) {
-    const node = new Node(value);
+  printList() {
+    const listNodes = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      listNodes.push(currentNode.data);
+      currentNode = currentNode.next;
+    }
+    return listNodes;
+  }
+
+  append(data) {
+    const node = new Node(data);
     if (!this.head) {
       this.head = node;
       this.tail = this.head;
@@ -25,8 +36,8 @@ class LinkedList {
     return this;
   }
 
-  prepend(value) {
-    const node = new Node(value);
+  prepend(data) {
+    const node = new Node(data);
     if (!this.head.next) {
       this.head = node;
       this.tail = this.head;
@@ -36,10 +47,47 @@ class LinkedList {
     this.length += 1;
     return this;
   }
+
+  get(index) {
+    if (index >= this.length || index < 0) return null;
+    let counter = 0;
+    let node = this.head;
+    while (counter !== index) {
+      node = node.next;
+      counter += 1;
+    }
+    return node;
+  }
+
+  insert(index, data) {
+    if (index > this.length || index < 0) return false;
+    if (index === this.length) return !!this.append(data);
+    if (index === 0) return !!this.prepend(data);
+    const newNode = new Node(data);
+    const previous = this.get(index - 1);
+    const pointer = previous.next;
+    previous.next = newNode;
+    newNode.next = pointer;
+    this.length += 1;
+    return this;
+  }
+
+  delete(index) {
+    let prev = this.get(index - 1);
+    // let next = this.get(index + 1);
+    let deletedNode = prev.next;
+    prev.next = deletedNode.next;
+    deletedNode.next = prev;
+    this.length -= 1;
+    return this;
+    // return ['prev ', prev, 'next ', next];
+  }
 }
 
 const myLinkedList = new LinkedList();
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
-console.log(myLinkedList);
+
+console.log(myLinkedList.delete(1));
+console.log(myLinkedList.printList());
