5275186bbc11060a009a9e5f6dafb249c73a2744
Masiu <ciszekmarcell@gmail.com>
2020-03-16 18:19:01 +0100

    insert node

--- data_structures2.0/linkedlists/LinkedListTs.ts
+++ data_structures2.0/linkedlists/LinkedListTs.ts
@@ -3,7 +3,7 @@ interface INode {
   next?: INode | null;
 }
 class Node_ {
-  data: number;
+  data: number | INode;
 
   next: INode | null;
 
@@ -40,6 +40,7 @@ class MyLinkedList {
     const newNode = new Node_(data);
     if (!this.head) {
       this.head = newNode;
+      this.tail = this.head;
     } else {
       this.head.next = newNode;
       this.tail = newNode;
@@ -47,10 +48,46 @@ class MyLinkedList {
     this.size += 1;
     return this;
   }
+
+  prepend(data) {
+    const newNode = new Node_(data);
+    if (!this.head.next) {
+      this.append(newNode);
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+    this.size += 1;
+    return this;
+  }
+
+  get(index) {
+    if (index > this.size || index < 0) return undefined;
+    let node = this.head;
+    let counter = 0;
+    while (counter !== index) {
+      node = node.next;
+      counter += 1;
+    }
+    return node;
+  }
+
+  insert(data, index) {
+    const newNode = new Node_(data);
+    const prev = this.get(index - 1);
+    console.log(prev);
+    const pointer = prev.next;
+    prev.next = newNode;
+    newNode.next = pointer;
+    this.size += 1;
+    return this;
+  }
 }
 
 const ll = new MyLinkedList();
 ll.append(6);
-ll.append(2);
 ll.append(3);
+ll.append(99);
+ll.prepend(5);
+ll.prepend(1);
+ll.insert(77, 2);
 console.log(ll.showNodes());
