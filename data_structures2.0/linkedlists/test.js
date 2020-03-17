1c4424ac61c23cb98c8db856347b5f075bb3c515
Masiu <ciszekmarcell@gmail.com>
2020-03-17 16:43:26 +0100

    pointers

--- data_structures2.0/linkedlists/test.js
+++ data_structures2.0/linkedlists/test.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 class Node {
   constructor(data) {
     this.data = data;
@@ -22,6 +23,10 @@ class MyLL {
     return nodesXS;
   }
 
+  /**
+   *
+   * @param {number} data
+   */
   // start
   prepend(data) {
     const newNode = new Node(data);
@@ -37,6 +42,10 @@ class MyLL {
   }
 
   // end
+  /**
+   *
+   * @param {number} data
+   */
   append(data) {
     const newNode = new Node(data);
     while (!this.head.next) {
@@ -47,10 +56,75 @@ class MyLL {
     this.size += 1;
     return this;
   }
+
+  /**
+   *
+   * @param {number} index
+   */
+  get(index) {
+    if (index > this.size || index < 0) return null;
+    let counter = 0;
+    let node = this.head;
+
+    while (counter !== index) {
+      node = node.next;
+      counter++;
+    }
+
+    // console.log(node.data);
+    return node;
+  }
+
+  /**
+   *
+   * @param {number} index
+   * @param {number} data
+   */
+  insert(index, data) {
+    if (index > this.size || index < 0) return null;
+    let previousNode = this.get(index - 1);
+    let newNode = new Node(data);
+    let pointer = previousNode.next;
+
+    previousNode.next = newNode;
+    newNode.next = pointer;
+    this.size += 1;
+
+    return this;
+  }
+
+  deleteHead() {
+    if (!this.head) return null;
+    let temp = this.head;
+    this.head = this.head.next;
+    temp = null;
+    return this;
+  }
+
+  /**
+   *
+   * @param {number} index
+   */
+  delete(index) {
+    if (index <= 0) {
+      this.deleteHead();
+      return;
+    }
+    let prevNode = this.get(index - 1);
+    let removedNode = prevNode.next;
+    prevNode.next = removedNode.next;
+    removedNode.next = prevNode;
+    this.size -= 1;
+    return this;
+  }
 }
 
 const myLL = new MyLL();
 myLL.prepend(5);
 myLL.prepend(2);
 myLL.append(1);
+myLL.append(3);
+// console.log(myLL.get(0));
+myLL.insert(1, 99);
+myLL.delete(0);
 console.log(myLL.printNodes());
