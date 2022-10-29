79efd0bb6e1e4abd0995879cf5610a9a9eacb8ab
masiucd <ciszekmarcell@gmail.com>
2022-10-29 16:58:15 +0200

    init

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const RootLayout = ({children}) => {
+  return (
+    <div>
+      RootLayout
+      {children}
+    </div>
+  )
+}
+
+export default RootLayout
---
+++ apps/web/app/page.tsx
@@ -0,0 +1,12 @@
+import {Button} from "ui"
+
+const HomePage = () => {
+  return (
+    <div>
+      HomePage
+      <Button />
+    </div>
+  )
+}
+
+export default HomePage
