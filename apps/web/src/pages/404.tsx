5ac46d8825393c72aca2e3062f87012df10ba6e7
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2022-11-17 17:31:45 +0100

    total reset

--- apps/web/src/pages/404.tsx
+++ apps/web/src/pages/404.tsx
@@ -1,23 +1,7 @@
-import Link from "next/link"
-import {ReactElement} from "react"
-
-import Layout from "~/components/layout"
-import Page from "~/components/wrappers/page"
-
 export default function NotFoundPage() {
-  return (
-    <Page title="Not found page">
-      <div className="p-5">
-        <h1>Ooops something went wrong</h1>
-        <div className="flex gap-5">
-          <Link href="/">Home</Link>
-          <Link href="/blog">Blog</Link>
-        </div>
-      </div>
-    </Page>
-  )
+  return <h1>asd</h1>
 }
 
-NotFoundPage.getLayout = function getLayout(page: ReactElement) {
-  return <Layout>{page}</Layout>
-}
+// NotFoundPage.getLayout = function getLayout(page: ReactElement) {
+//   return <Layout>{page}</Layout>
+// }
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,17 +1,7 @@
-import {ReactElement} from "react"
-
-import Intro from "~/components/home/intro"
-import Layout from "~/components/layout"
-import Page from "~/components/wrappers/page"
-
 export default function HomePage() {
-  return (
-    <Page>
-      <Intro />
-    </Page>
-  )
+  return <h1>asdasd</h1>
 }
 
-HomePage.getLayout = function getLayout(page: ReactElement) {
-  return <Layout>{page}</Layout>
-}
+// HomePage.getLayout = function getLayout(page: ReactElement) {
+//   return <Layout>{page}</Layout>
+// }
