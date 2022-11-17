7914a82ab89ab741a3a60b558c22d987041a9d15
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2022-11-17 17:16:03 +0100

    total reset

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -10,32 +10,6 @@ export default function Document() {
           name="viewport"
           content="width=device-width, initial-scale=1.0, viewport-fit=cover"
         />
-        {/* <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
-        <link href="/static/favicons/site.webmanifest" rel="manifest" />
-        <link
-          href="/static/favicons/apple-touch-icon.png"
-          rel="apple-touch-icon"
-          sizes="180x180"
-        />
-        <link
-          href="/static/favicons/favicon-32x32.png"
-          rel="icon"
-          sizes="32x32"
-          type="image/png"
-        />
-        <link
-          href="/static/favicons/favicon-16x16.png"
-          rel="icon"
-          sizes="16x16"
-          type="image/png"
-        />
-        <link
-          color="#4a9885"
-          href="/static/favicons/safari-pinned-tab.svg"
-          rel="mask-icon"
-        />s
-        /> */}
-        <meta content="14d2e73487fa6c71" name="yandex-verification" />
         <meta content="#ffffff" name="theme-color" />
         <meta content="#ffffff" name="msapplication-TileColor" />
       </Head>
