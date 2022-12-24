47bc4382081631a7a24da7a3e234ca3352b54975
masiucd <ciszekmarcell@gmail.com>
2022-12-24 17:25:41 +0100

    Update next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      "pbs.twimg.com", // Twitter Profile Picture
+      "cdn.sanity.io",
+    ],
+  },
   experimental: {
     transpilePackages: ["ui", "lib"],
     fontLoaders: [{loader: "@next/font/google", options: {subsets: ["latin"]}}],
